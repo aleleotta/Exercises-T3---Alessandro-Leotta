c6c295355144cb156cb5d22ad8977dfd40072845
aleleotta <aleleotta@106-02.info2>
2022-11-10 13:17:42 +0100

    Exercise 1 initialized!

---
+++ Exercises T3 - Alessandro Leotta/src/exercises/Exercise1.java
@@ -0,0 +1,22 @@
+package exercises;
+import java.util.*;
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		int hours;
+		int minutes;
+		int seconds;
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		sc.close();
+	}
+}
+/*Escribe un programa que incremente la hora de un reloj. Se pedirán por teclado:
+Hora
+Minutos
+Segundos
+Cantidad de segundos a incrementar
+La aplicación debe mostrar la nueva hora. 
+Por ejemplo, si el usuario introduce hora=13, minutos=59 y segundos=51, 
+y se desea incrementar en 10 segundos, el resultado a mostrar es 14:00:01.
+*/
