6df892f895889ab17fea6703a3d271c8ec0623e6
aleleotta <aleleotta@106-02.info2>
2022-11-17 14:41:38 +0100

    Exercise 9 complete.

--- Exercises T3 - Alessandro Leotta/src/exercises/Exercise9.java
+++ Exercises T3 - Alessandro Leotta/src/exercises/Exercise9.java
@@ -3,7 +3,29 @@ import java.util.*;
 
 public class Exercise9 {
 	public static void main(String[] args) {
+		int i;
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce a number: ");
+		i = sc.nextInt();
+		if (i <= 9) {
+			System.out.println("The following number has 1 digit.");
+		} else if (i > 9 && i <= 99) {
+			System.out.println("The following number has 2 digits.");
+		} else if (i > 99 && i <= 999) {
+			System.out.println("The following number has 3 digits.");
+		} else if (i > 999 && i <= 9999) {
+			System.out.println("The following number has 4 digits.");
+		} else if (i > 9999 && i <= 99999) {
+			System.out.println("The following number has 5 digits.");
+		} else if (i > 99999 && i <= 999999) {
+			System.out.println("The following number has 6 digits.");
+		} else if (i > 999999 && i <= 9999999) {
+			System.out.println("The following number has 7 digits.");
+		} else if (i > 9999999 && i <= 99999999) {
+			System.out.println("The following number has 8 digits.");
+		} else if (i > 99999999 && i <= 999999999) {
+			System.out.println("The following number has 9 digits.");
+		}
 		sc.close();
 	}
 }
