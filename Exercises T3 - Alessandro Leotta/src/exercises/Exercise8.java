8d6a80c13c2d82025dbd782425520f3e624802ac
aleleotta <aleleotta@106-02.info2>
2022-11-17 14:20:45 +0100

    Exercise 8 done and documented!

--- Exercises T3 - Alessandro Leotta/src/exercises/Exercise8.java
+++ Exercises T3 - Alessandro Leotta/src/exercises/Exercise8.java
@@ -3,7 +3,36 @@ import java.util.*;
 
 public class Exercise8 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		int inputN; //Number that is introduced by the user.
+		int secretN; //Secret number of the dice
+		int counter; //Amount of times the user introduces a number into the program.
+		int fails = 0; //Numbers that are less than the secret number
+		Scanner sc = new Scanner(System.in); //Scanner declared
+		System.out.println("*Introduce 0 to stop running the program.\nMake sure the number is between 0 and 100.*"); //Instructions to the user.
+		System.out.print("Introduce a number: ");
+		inputN = sc.nextInt(); //First user input
+		if (inputN <= 0 || inputN > 100) { //If the user all of the sudden enters an invalid value as the first value, the program will terminate immediately. This if block will prevent the 0 from being counted as an introduced number.
+			System.out.println("ERROR!");
+		}
+		counter = 1; //Counter increments for the first time.
+		for (; inputN > 0; counter++) { //Start of for loop
+			if (inputN < 0 || inputN > 100) { //Block that will prevent numbers outside the interval from being counted.
+				System.out.println("Values have to be between 0 and 100. This value is invalid.");
+				counter--;
+				System.out.print("Introduce a number: ");
+				inputN = sc.nextInt();
+				continue;
+			}
+			secretN = (int) (Math.random() * 100); //Randomizer
+			if(secretN > inputN) { //If the number is less than the secret number then the fails counter will increment.
+				System.out.println("This number is less than the secret number on the dice. Fail indicator incremented.\n");
+				fails++;
+			}
+			System.out.print("\nIntroduce a number: ");
+			inputN = sc.nextInt();
+		}
+		System.out.println("Numbers introduced: " + counter); //Final results and termination
+		System.out.println("Fails: " + fails);
 		sc.close();
 	}
 }
